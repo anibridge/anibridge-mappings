9282120a7c5a9c84097c0e6a9fc0662b8d25f258
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-01-17 01:37:55 +0000

    chore: Setup a basic package structure

--- src/anibridge_mappings/__init__.py
+++ src/anibridge_mappings/__init__.py
@@ -1,1 +1,10 @@
 """AniBridge mappings package."""
+
+import importlib.metadata
+
+__author__ = "Elias Benbourenane <eliasbenbourenane@gmail.com>"
+__credits__ = ["eliasbenb", "LuceoEtzio"]
+__license__ = "MIT"
+__maintainer__ = "eliasbenb"
+__email__ = "eliasbenbourenane@gmail.com"
+__version__ = importlib.metadata.version("anibridge-mappings")
